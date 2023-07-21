c57ad5c0a6712e44374efb18c57dff866c6b7fed
Kewal Wadibhasme <hello@iamkewal.com>
2023-07-21 20:02:41 +1000

    add playwright for TDD

---
+++ e2e/ex.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from "@playwright/test";
+
+test("should detect welcome text", async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto("http://localhost:3000/");
+
+  await expect(page.locator("h1")).toContainText("Welcome to Next.js");
+});
